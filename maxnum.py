270eff047b6bfd00e20ad976cd42e3e7f288c42d
saishanker41 <69297472+saishanker41@users.noreply.github.com>
2020-08-06 17:30:14 +0530

    Create maxnum.py

---
+++ maxnum.py
@@ -0,0 +1,10 @@
+def maxnum(n):
+n = int(input("Enter the size of an array"))
+list = [int(input()) for x in range(n)]
+max = list[0]
+for i in range(0,len(list)):
+if list[i] > max:
+max = list[i]
+
+print("max num in the given list is ", +str(max)
+print(maxnum(5)
